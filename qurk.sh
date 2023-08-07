3a68531360611163194abbfca7b400879c46b07f
Ureakim <dev@ureakim.net>
2023-08-08 00:24:42 +0200

    build firmware via script instead of Makefile

--- qurk.sh
+++ qurk.sh
@@ -8,7 +8,7 @@ function defaultargs() {
 	QMK_KM=default
 }
 
-function usage(){
+function usage() {
 	printf "Usage :\n"
 	printf "\t--bl                   : QMK bootloader name ;\n"
 	printf "\t--kb                   : QMK keyboard name ;\n"
@@ -17,21 +17,36 @@ function usage(){
 }
 
 function checkargs() {
-	if [ -z  "$QMK_KB" ]
-	then
-		>&2 echo "no --kb argument: keyboard name is mandatory."
+	if [ -z "$QMK_KB" ]; then
+		echo >&2 "no --kb argument: keyboard name is mandatory."
 		exit 1
 	fi
 
-	if [ -z  "$QMK_KR" ]
-	then
-		>&2 echo "no --kr argument: keyboard model is mandatory."
+	if [ -z "$QMK_KR" ]; then
+		echo >&2 "no --kr argument: keyboard model is mandatory."
 		exit 1
 	fi
 }
 
 function loadargs() {
+	if [ ! -f "$QMK_ENV/.env" ]; then
+		echo >&2 "no .env file in $QMK_ENV"
+		exit 1
+	fi
+
 	. $QMK_ENV/.env
+}
+
+function firmwarename() {
+	case $QMK_BL in
+	liatris)
+		QMK_FW="${QMK_KB//\//_}_${QMK_KR}_${QMK_KM}_${QMK_BL}.uf2"
+		;;
+	*)
+		echo >&2 "file extension unknown for target $QMK_BL"
+		exit 1
+		;;
+	esac
 }
 
 function setup() {
@@ -42,64 +57,80 @@ function setup() {
 }
 
 function clean() {
-	rm -rf .build
+	rm -rf firmware/*
 }
 
 function build() {
 	docker run -it --rm \
 		-v qmk_firmware:/qmk_firmware \
-		-v ./.build:/qmk_firmware/.build \
 		-v ./keyboards/$QMK_KB/keymaps/$QMK_KM:/qmk_firmware/keyboards/$QMK_KB/keymaps/$QMK_KM \
-		-w /qmk_firmware \
+		-v ./firmware:/firmware \
+		-w /firmware \
 		qmkfm/qmk_cli:latest \
 		bash -c "qmk compile -e CONVERT_TO=$QMK_BL -kb $QMK_KB/$QMK_KR -km $QMK_KM && \
-		chown -R $(id -u):$(id -g) /qmk_firmware/.build"
+			cp /qmk_firmware/$QMK_FW /firmware && \
+			chown -R $(id -u):$(id -g) /firmware"
 }
 
-defaultargs
-
-OPTS=$( getopt -o h -l setup,clean,bl:,kb:,km:,env: -- "$@" )
-if [ $? != 0 ]
-then
+OPTS=$(getopt -o h -l setup,clean,bl:,kb:,km:,env: -- "$@")
+if [ $? != 0 ]; then
 	exit 1
 fi
 
 eval set -- "$OPTS"
 
-while true ; do
+while true; do
 	case "$1" in
-		-h) usage;
-			exit 0;;
-		--bl) QMK_BL=$2;
-			shift 2;;
-		--kb) QMK_KB=$2;
-			shift 2;;
-		--km) QMK_KM=$2;
-			shift 2;;
-		--kr) QMK_KR=$2;
-			shift 2;;
-		--env) QMK_ENV=$2;
-			shift 2;;
-		--setup) setup;
-			exit 0;;
-		--clean) clean;
-			exit 0;;
-		--) shift; break;;
+	-h)
+		usage
+		exit 0
+		;;
+	--bl)
+		QMK_BL=$2
+		shift 2
+		;;
+	--kb)
+		QMK_KB=$2
+		shift 2
+		;;
+	--km)
+		QMK_KM=$2
+		shift 2
+		;;
+	--kr)
+		QMK_KR=$2
+		shift 2
+		;;
+	--env)
+		QMK_ENV=$2
+		shift 2
+		;;
+	--setup)
+		setup
+		exit 0
+		;;
+	--clean)
+		clean
+		exit 0
+		;;
+	--)
+		shift
+		break
+		;;
 	esac
 done
 
+defaultargs
 # if the env var is set, ignore other vars and get them from the env file
-if [ -z "$QMK_ENV" ]
-then
-	checkargs
-else
-	defaultargs
+if [ ! -z "$QMK_ENV" ]; then
 	loadargs
-	checkargs
 fi
+checkargs
+firmwarename
 
-echo "BL: $QMK_BL"
 echo "KB: $QMK_KB"
-echo "KM: $QMK_KR"
+echo "KR: $QMK_KR"
 echo "KM: $QMK_KM"
+echo "BL: $QMK_BL"
+echo "FW: $QMK_FW"
 build
