df8646fb234b24c46d166f64b07071e77d2e8742
Ureakim <dev@ureakim.net>
2023-08-23 23:00:45 +0200

    fix signs hhkb

--- hhkb_pro2/keymap.c
+++ hhkb_pro2/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		XXXXXXX,		KC_F1,			KC_F2,			KC_F3,			KC_F4,			KC_F5,			KC_F6,			KC_F7,			KC_F8,			KC_F9,			KC_F10,			KC_F11,			KC_F12,			XXXXXXX,		XXXXXXX,
 		_______,		KC_1,				KC_2,				KC_3,				KC_4,				KC_5,				KC_6,				KC_7,				KC_8,				KC_9,				KC_0,				XXXXXXX,		XXXXXXX,		KC_DEL,
 		_______,		KC_EXLM,		KC_AT,			KC_HASH,		KC_DLR,			KC_PERC,		KC_CIRC,		KC_AMPR,		KC_ASTR,		KC_LPRN,		KC_RPRN,		KC_PIPE,		_______,
-		_______,		KC_LT,			KC_GT,			XXXXXXX,		KC_EQL,			KC_PEQL,		KC_PSLS,		KC_PPLS,		KC_PMNS,		KC_LBRC,		KC_RBRC,		_______,		XXXXXXX,
+		_______,		KC_SLSH,		XXXXXXX,		KC_TILD,		KC_LT,			KC_GT,			KC_BSLS,		KC_EQL,			KC_MINS,		KC_LBRC,		KC_RBRC,		_______,		XXXXXXX,
 		/*										*/_______,		XXXXXXX,														_______,														_______,		_______
 	),
 
@@ -39,4 +39,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_CAPS,		LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), XXXXXXX,		XXXXXXX,		XXXXXXX,		XXXXXXX,		XXXXXXX,		XXXXXXX,		KC_CAPS,		XXXXXXX,
 		/*										*/_______,		_______,														_______,														XXXXXXX,		_______
 	)
+};
+
+// key override
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL
 };
---
+++ hhkb_pro2/readme.md
@@ -0,0 +1,18 @@
+# HHKB pro 2
+
+## Layers
+
+**0 _BASE**
+<p align="center">
+  <img src="https://raw.githubusercontent.com/Ureakim/qmk_keymaps/master/hhkb_pro2/assets/_base.png">
+</p>
+
+**1 _SIGNS**
+<p align="center">
+  <img src="https://raw.githubusercontent.com/Ureakim/qmk_keymaps/master/hhkb_pro2/assets/_signs.png">
+</p>
+
+**2 _MOVE**
+<p align="center">
+  <img src="https://raw.githubusercontent.com/Ureakim/qmk_keymaps/master/hhkb_pro2/assets/_move.png">
+</p>
