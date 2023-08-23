61d3bfa4c92787527f2c1f6ce7554b595dd5dc68
Ureakim <dev@ureakim.net>
2023-08-23 22:00:04 +0200

    fix signs sofle_v2

--- sofle_v2/keymap.c
+++ sofle_v2/keymap.c
@@ -27,11 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
 		/*                    */KC_LGUI,    KC_LALT,    KC_LCTL,    MO(_MOVE),  KC_SPC,     KC_ENT,     MO(_SIGNS), KC_RCTL,    KC_RALT,    KC_RGUI
 	),
+
 	[_SIGNS] = LAYOUT(
 		KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                              KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-		_______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
-		_______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_PIPE,
-		_______,    KC_LT,      KC_GT,      XXXXXXX,    KC_EQL,     KC_PEQL,    _______,    _______,    KC_PSLS,    KC_PPLS,    KC_PMNS,    KC_LBRC,    KC_RBRC,    _______,
+		_______,    KC_1,				KC_2,	      KC_3,				KC_4,	      KC_5,	                              KC_6,	      KC_7,	      KC_8,	      KC_9,	      KC_0,				KC_BSPC,
+		_______,    KC_EXLM,		KC_AT,			KC_HASH,		KC_DLR,			KC_PERC,                            KC_CIRC,    KC_AMPR,		KC_ASTR,		KC_LPRN,		KC_RPRN,		KC_BSLS,
+		_______,    KC_SLSH,    XXXXXXX,		KC_TILD,    KC_LT,	    KC_GT,	    _______,    _______,    KC_BSLS,    KC_EQL,			KC_MINS,    KC_LBRC,		KC_RBRC,		_______,
 		/*                    */_______,    _______,    _______,    XXXXXXX,    _______,    _______,    _______,    _______,    _______,    _______
 	),
 	[_MOVE] = LAYOUT(
@@ -49,8 +50,17 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 	[_SIGNS] =  { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(XXXXXXX, XXXXXXX)  },
 	[_MOVE]  =  { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(XXXXXXX, XXXXXXX)  }
 };
-
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+// key override
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL
+};
+
 void keyboard_pre_init_user(void) {
 	// turning off the onboard power LED
 	setPinOutput(24);
